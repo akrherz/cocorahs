4b76500e735d3c3ee5b81ebe0180b0552d80be4c
akrherz <akrherz@95f8c243-6001-0410-b151-932e6a9ed213>
2007-08-20 18:35:21 +0000

    Come on

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,6 +1,6 @@
 <?php
-//$rooturl = "http://iowa.cocorahs.org";
-$rooturl = "http://localhost/cocorahs/";
-$rootpath = "/home/akrherz/projects/cocorahs/";
-//$rootpath = "/mesonet/www/apps/cocorahs/";
+$rooturl = "http://iowa.cocorahs.org";
+//$rooturl = "http://localhost/cocorahs/";
+//$rootpath = "/home/akrherz/projects/cocorahs/";
+$rootpath = "/mesonet/www/apps/cocorahs/";
 ?>
